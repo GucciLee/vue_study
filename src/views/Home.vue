922ed43f673d08650ebe43ff6666694fcfef1537
Gucci <GucciLee@163.com>
2019-04-10 23:21:50 +0800

    初始化 vuee

---
+++ src/views/Home.vue
@@ -0,0 +1,26 @@
+<template>
+    <div id="nav">
+        <template>
+            <router-link to="example">演示 Demo</router-link>
+        </template>
+    </div>
+</template>
+
+<script>
+    export default {
+        name: "Home"
+    };
+</script>
+
+<style lang="less">
+    #nav {
+        padding: 30px;
+        a {
+            font-weight: bold;
+            color: #2c3e50;
+            &.router-link-exact-active {
+                color: #42b983;
+            }
+        }
+    }
+</style>
---
+++ src/views/example/index.vue
@@ -0,0 +1,13 @@
+<template>
+    <h1>我是演示程序</h1>
+</template>
+
+<script>
+    export default {
+        name: "index"
+    }
+</script>
+
+<style scoped>
+
+</style>
